db86b2cb4e59432d0168f416a1fdcc089dea5ec4
falufri-te20-Vidar <te20vidale@falufri.se>
2022-03-28 12:02:06 +0200

    Added game

---
+++ Water-Game1/Assets/Code/Movement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private KeyCode jumpKey;
+    [SerializeField] private KeyCode moveLeftKey;
+    [SerializeField] private KeyCode moveRightKey;
+
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Rigidbody2D playerRigidBody;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float jumpForce;
+
+    private bool canJump = false;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            if (Groundcheck())
+                playerRigidBody.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (Input.GetKey(moveLeftKey))
+        {
+
+        }
+
+        if (Input.GetKey(moveRightKey))
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+    private bool Groundcheck()
+    {
+        var collision = Physics2D.OverlapBox(player.transform.position + new Vector3(0f, -0.1f, 0f), player.transform.localScale, 0f);
+
+        if (collision == null)
+            return false;
+
+        if (collision.IsTouchingLayers(groundMask))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
